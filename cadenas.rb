c981ed8eff426187d63e4de612e2fd6fbfa63923
jonathan <jonathang_25@hotmail.com>
2013-12-26 16:38:53 -0430

    commit final

--- cadenas.rb
+++ cadenas.rb
@@ -1,18 +1,18 @@
 # Salvar como guess.rb
-words = ['foobar', 'baz', 'quux']
-secret = words[rand(3)]
-print "adivina? "
-while guess = STDIN.gets
-guess.chop!
-if guess == secret
-print "Ganas\n"
-break
-else
-print "Lo siento. Pierdes\n"
-end
-print "adivina? "
-end
-print "La palabra era ", secret, ".\n"
+#words = ['foobar', 'baz', 'quux']
+#secret = words[rand(3)]
+#print "adivina? "
+#while guess = STDIN.gets
+#guess.chop!
+#if guess == secret
+#print "Ganas\n"
+#break
+#else
+#print "Lo siento. Pierdes\n"
+#end
+#print "adivina? "
+#end
+#print "La palabra era ", secret, ".\n"
 
 h = {"uno" => 1, "dos" => 2, "tres" => 3}
 puts h["dos"]
@@ -26,3 +26,32 @@ case i
 	else
 		print "11..100\n"
 end
+
+case 'abcdef'
+	when 'aaa', 'bbb'
+		print "aaa o bbb\n"
+	when /def/
+		print "incluye /def/\n"
+end
+
+for num in (4..6)
+	print num, "\n"
+end
+
+for elm in [100,-9.6,"pickle"]
+   print "#{elm}\t()\n"
+end
+
+"abc".each_byte{|c| printf"{%c}", c}; print "\n"
+
+"a\nb\nc\n".each_line{|l| print l}
+
+
+def repeat(num)
+  while num > 0
+    yield
+    num -= 1
+  end
+end
+
+repeat(3){ print "foo\n" }
