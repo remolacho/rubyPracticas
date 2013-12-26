0b7e389d94f4b66c2d1760d9fe1e4783b208cc00
jonathan <jonathang_25@hotmail.com>
2013-12-26 11:27:59 -0430

    Se modifico el archivo

--- cadenas.rb
+++ cadenas.rb
@@ -13,3 +13,16 @@ end
 print "adivina? "
 end
 print "La palabra era ", secret, ".\n"
+
+h = {"uno" => 1, "dos" => 2, "tres" => 3}
+puts h["dos"]
+
+i=12
+case i
+	when 1, 2..5 then
+ 		print "1..5\n"
+	when 6..10 then
+		print "6..10\n"
+	else
+		print "11..100\n"
+end
